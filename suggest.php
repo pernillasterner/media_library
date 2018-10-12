8292af09d01a8be36c96d28c34a25b1f73967e76
psterner <pernilla.sterner@chas.se>
2018-10-12 18:15:44 +0200

    added SMTP - username, pwd not working

--- suggest.php
+++ suggest.php
@@ -14,6 +14,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 require 'vendor/phpmailer/phpmailer/src/PHPMailer.php';
 require 'vendor/phpmailer/phpmailer/src/Exception.php';
+require 'vendor/phpmailer/phpmailer/src/SMTP.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   $name = trim(filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING));
@@ -42,13 +43,36 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 
   $mail = new PHPMailer;
+  // SMTP Service
+  $mail->isSMTP();
+  //Enable SMTP debugging
+  // 0 = off (for production use)
+  // 1 = client messages
+  // 2 = client and server messages
+  $mail->SMTPDebug = 2;
+  //Set the hostname of the mail server
+  $mail->Host = 'smtp.gmail.com';
+  // use
+  // $mail->Host = gethostbyname('smtp.gmail.com');
+  // if your network does not support SMTP over IPv6
+  //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+  $mail->Port = 587;
+  //Set the encryption system to use - ssl (deprecated) or tls
+  $mail->SMTPSecure = 'tls';
+  //Whether to use SMTP authentication
+  $mail->SMTPAuth = true;
+  //Username to use for SMTP authentication - use full email address for gmail
+  $mail->Username = "email@com";
+  //Password to use for SMTP authentication
+  $mail->Password = "pwd";
+
   //It's important not to use the submitter's address as the from address as it's forgery,
   //which will cause your messages to fail SPF checks.
   //Use an address in your own domain as the from address, put the submitter's address in a reply-to
-  $mail->setFrom('psterner@hotmail.com', $name);
+  $mail->setFrom('email@com', $name);
   $mail->addReplyTo($email, $name);
-  $mail->addAddress('psterner@hotmail.com', 'Pernilla');
-  $mail->Subject = 'Library Suggeston from ' . $name;
+  $mail->addAddress('email@com', 'username');
+  $mail->Subject = 'Library Suggestion from ' . $name;
   $mail->Body = $email_body;
   if (!$mail->send()) {
       echo "Mailer Error: " . $mail->ErrorInfo;
